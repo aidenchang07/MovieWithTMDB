9ebab8adb6a87ab3fd2e1e23092f0b94ccc7acc5
Aiden Chang <aidenchang@pcpartner.com>
2024-02-01 20:32:14 +0800

    Init project

---
+++ app/src/main/java/com/adncoding/moviewithtmdb/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.adncoding.moviewithtmdb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.adncoding.moviewithtmdb.ui.theme.MovieWithTMDBTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MovieWithTMDBTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MovieWithTMDBTheme {
+        Greeting("Android")
+    }
+}
